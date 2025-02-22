b7604d3afc817cd47a2b55d0460aa89d10e7f832
MatteyGG <matgaranin@gmail.com>
2025-02-22 20:44:59 +0300

    Added vk support

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { visit } from 'unist-util-visit';
 const DEFAULT_WIDTH = 560;
 const DEFAULT_HEIGHT = 315;
 const URL_PATTERN = /^https:\/\/(?:youtu\.be\/|www\.youtube\.com\/watch\?v=)([0-9A-Za-z_-]+)$/;
-
+const VK_URL_PATTERN = /^https:\/\/vk\.com\/video_ext\.php\?id=(\d+)(?:&hash=[0-9a-f]+)?$/;
+// EXample of URL for vk "https://vk.com/video_ext.php?oid=-229290760&id=456239018&hd=2&hash=8a8f5bbbcfb49917&autoplay=1"
 interface Options {
   width?: number;
   height?: number;
@@ -19,10 +20,13 @@ const remarkYoutubePlugin = (options?: Options) => (tree: Root) => {
       if (child.type === 'text' || child.type === 'link') {
         const url = (child as Link)?.url ?? (child as Text)?.value;
         const match = url.match(URL_PATTERN);
+        const vkMatch = url.match(VK_URL_PATTERN);
         if (match && match[1]) {
           videoId = match[1];
           videoUrl = url;
-          break;
+        } else if (vkMatch && vkMatch[1]) {
+          videoId = vkMatch[1];
+          videoUrl = url;
         }
       }
     }
@@ -36,7 +40,7 @@ const remarkYoutubePlugin = (options?: Options) => (tree: Root) => {
           hProperties: {
             width: options?.width ?? DEFAULT_WIDTH,
             height: options?.height ?? DEFAULT_HEIGHT,
-            src: `https://www.youtube.com/embed/${videoId}`,
+            src: videoId.startsWith('https://vk.com') ? videoUrl : `https://www.youtube.com/embed/${videoId}`,
             frameborder: '0',
             allow:
               'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
@@ -51,3 +55,4 @@ const remarkYoutubePlugin = (options?: Options) => (tree: Root) => {
 };
 
 export default remarkYoutubePlugin;
+

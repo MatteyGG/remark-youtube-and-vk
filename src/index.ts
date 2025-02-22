d0b2cc98e99635122233ff134ef1f0eec13f7eeb
MatteyGG <matgaranin@gmail.com>
2025-02-22 21:17:29 +0300

    synergy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import type { Root, Text, Link } from 'mdast';
 import { visit } from 'unist-util-visit';
+
 const DEFAULT_WIDTH = 560;
 const DEFAULT_HEIGHT = 315;
-const URL_PATTERN = /^https:\/\/(?:youtu\.be\/|www\.youtube\.com\/watch\?v=)([0-9A-Za-z_-]+)$/;
+const YOUTUBE_URL_PATTERN = /^https:\/\/(?:youtu\.be\/|www\.youtube\.com\/watch\?v=)([0-9A-Za-z_-]+)$/;
 const VK_URL_PATTERN = /^https:\/\/vk\.com\/video_ext\.php\?.*oid=.*&id=.*&hash=.*/;
 
 interface Options {
@@ -9,62 +11,32 @@ interface Options {
   height?: number;
 }
 
-const remarkYoutubePlugin = (options?: Options) => (tree: Root) => {
+const remarkVideoPlugin = (options?: Options) => (tree: Root) => {
   visit(tree, 'paragraph', (node) => {
-    let videoId = '';
     let videoUrl = '';
+    let iframeSrc = '';
+
     for (const child of node.children) {
-      // parse type = 'link' for 'remark-gfm'
       if (child.type === 'text' || child.type === 'link') {
         const url = (child as Link)?.url ?? (child as Text)?.value;
-        const match = url.match(URL_PATTERN);
-        const vkMatch = url.match(VK_URL_PATTERN);
-        if (match && match[1]) {
-          videoId = match[1];
+        const youtubeMatch = url.match(YOUTUBE_URL_PATTERN);
+        if (youtubeMatch && youtubeMatch[1]) {
+          iframeSrc = `https://www.youtube.com/embed/${youtubeMatch[1]}`;
           videoUrl = url;
-        } else if (vkMatch) {
-          videoUrl = url;
+          break;
         }
-      }
-    }
 
-    if (videoId || videoUrl) {
-      const text: Text = {
-        type: 'text',
-        value: videoUrl,
-        data: {
-          hName: 'iframe',
-          hProperties: {
-            width: options?.width ?? DEFAULT_WIDTH,
-            height: options?.height ?? DEFAULT_HEIGHT,
-            src: videoId ? `https://www.youtube.com/embed/${videoId}` : videoUrl,
-            frameborder: '0',
-            allow:
-              'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
-            allowfullscreen: true,
-          },
-          hChildren: [],
-        },
-      };
-      node.children = [text];
-    }
-  });
-};
-
-const remarkVkPlugin = (options?: Options) => (tree: Root) => {
-  visit(tree, 'paragraph', (node) => {
-    let videoUrl = '';
-    for (const child of node.children) {
-      if (child.type === 'link') {
-        const url = child.url;
         const vkMatch = url.match(VK_URL_PATTERN);
         if (vkMatch) {
+          iframeSrc = url;
           videoUrl = url;
+          break;
         }
       }
     }
-    if (videoUrl) {
-      const iframeNode = {
+
+    if (iframeSrc && videoUrl) {
+      const iframeNode: Text = {
         type: 'text',
         value: videoUrl,
         data: {
@@ -72,7 +44,7 @@ const remarkVkPlugin = (options?: Options) => (tree: Root) => {
           hProperties: {
             width: options?.width ?? DEFAULT_WIDTH,
             height: options?.height ?? DEFAULT_HEIGHT,
-            src: videoUrl,
+            src: iframeSrc,
             frameborder: '0',
             allow:
               'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
@@ -86,5 +58,5 @@ const remarkVkPlugin = (options?: Options) => (tree: Root) => {
   });
 };
 
-export { remarkYoutubePlugin, remarkVkPlugin };
+export default remarkVideoPlugin;
 

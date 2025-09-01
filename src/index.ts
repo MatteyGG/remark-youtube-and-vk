fb04e1fda9e2c669376495f893102b171e7906a5
MatteyGG <49521885+MatteyGG@users.noreply.github.com>
2025-09-01 14:56:51 +0300

    Added new domain for vkvideo.ru

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { visit } from 'unist-util-visit';
 const DEFAULT_WIDTH = 560;
 const DEFAULT_HEIGHT = 315;
 const YOUTUBE_URL_PATTERN = /^https:\/\/(?:youtu\.be\/|www\.youtube\.com\/watch\?v=)([0-9A-Za-z_-]+)$/;
-const VK_URL_PATTERN = /^https:\/\/vk\.com\/video_ext\.php\?.*oid=.*&id=.*&hash=.*/;
+const VK_URL_PATTERN = /^https:\/\/(?:vk\.com|vkvideo\.ru)\/video_ext\.php\?.*oid=.*&id=.*&hash=.*/;
 
 interface Options {
   width?: number;
